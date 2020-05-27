636070d13a8c36fb39a426aed76d2e0b123a433d
GabrielaNiemcewicz <gabriela.niemcewicz@ucdconnect.ie>
2020-05-27 23:24:55 +0200

    Initial classes structure

---
+++ Person.java
@@ -0,0 +1,29 @@
+
+public class Person {
+
+	boolean infected;
+	boolean susceptible;
+	//Work
+	double exposure;
+	int age;
+	Character character;
+	
+	Person(){
+		susceptible = true;
+		infected = false;
+	}
+	
+	
+	public void socialize() {}
+	
+	public void go_to_work() {}
+	
+	public void do_groceries() {}
+	
+	public void exercise() {}
+	
+	public void get_exposed() {}
+	
+	public int infect() {return 0;}
+	
+}
