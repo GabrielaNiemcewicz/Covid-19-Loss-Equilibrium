1f157e21c96b8cee6fe90847299cd995a37542b6
GabrielaNiemcewicz <gabriela.niemcewicz@ucdconnect.ie>
2020-05-29 13:44:39 +0200

    Basic mechanics of transmition, city infrastructure

--- Covid.java
+++ Covid.java
@@ -2,7 +2,56 @@
 public enum Covid {
 	int [] drate = new int [] {1,3,10}; //in promiles
 	double TRANSMITION_RATE = 2.4;
-	int [] TIME_TO_RECOVERY = new int [] {14,24}; //days
+	int [] TIME_TO_RECOVERY = new int [] {12,22}; //days
+	int [] TIME_TO_CONTAGIOUS = new int [] {1,5}; //days
 	
-
+	
+	
+ public	double howContagious(int time_iterator, Person vector) {
+	 double distance =1; //hard-coded for now
+	int t = i; //timeline - all project iterator?
+	//definitely each Person's quality
+	 int t_to_contagious = (int) java.lang.Math.random()*(TIME_TO_CONTAGIOUS[1]-TIME_TO_CONTAGIOUS[0])+TIME_TO_CONTAGIOUS[0];
+	 int t_to_recovery = (int) java.lang.Math.random()*(TIME_TO_RECOVERY[1]-TIME_TO_RECOVERY[0])+TIME_TO_RECOVERY[0];
+	if(time_iterator<t_to_contagious)
+		return 0;
+	if(time_iterator>=t_to_recovery)
+		{vector.setToRecovered();
+		return 0;
+		}
+	if(distance>9)
+		return 0;
+	return 1/java.lang.Math.pow(distance, -2); //spheres' areas of diff radius
+	 
+ }
+ 
+ 
+ public void is_Exposed(/*ArrayList*/Person vector) {
+	 //if 2metres for 15 minutes, at risk 
+	 final double AT_RISK = 15*3*1/java.lang.Math.pow(2, -2);  //3 steps in a minute //possible enum
+	 //develop analogical scenerios
+	 int sum = 0; //might be in Person, and decrement with time -> too resource-exhausting, maybe at end of each day
+	 if(vector.contagious)
+		/*this*/ sum+=howContagious(10,vector);
+	 if(sum>AT_RISK)
+		 setInitExposure();
+		 	 
+	 //possibly-wind, aired
+ }
+ 
+ private void setInitExposure() {
+	 double exposureRisk = 0.3;
+ }
+ 
+ //use only at the end of time periods
+public void getInfected () {
+	
+}
+//precondition: exposureRisk>initExposure
+public double increaseRisk () {
+//	double exposureRisk = 0.3 *sum/AT_RISK;
+}
+ 
+	
+	
 }
